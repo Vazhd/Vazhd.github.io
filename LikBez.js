eaec6029e67d7359e656075d9dc798402be336af
Vazhd <89795391+Vazhd@users.noreply.github.com>
2022-02-03 12:17:49 +0600

    Add files via upload

--- LikBez.js
+++ LikBez.js
@@ -5,3 +5,22 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 })
+//modal items
+const modals = document.getElementById('email-modals');
+const openBtn = document.querySelector('.main-btn');
+const closeBtn = document.querySelector('.close-btn');
+
+//click events 
+openBtn.addEventListener('click', ()=>{
+    modals.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', ()=>{
+    modals.style.display = 'none';
+});
+
+window.addEventListener('click', (e)=>{
+    if(e.target === modals){
+        modals.style.display= 'none';
+    }
+});
